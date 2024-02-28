b4462a6ff69ce282e2aa6e30aa859e9f5e4a48e9
xi4 <beelzebul.18@gmail.com>
2024-02-28 22:47:53 +0300

    Изменения в структуре Word и функции AddWord и AddPage

--- internal/db/db.go
+++ internal/db/db.go
@@ -7,8 +7,9 @@ import (
 
 type Word struct {
 	gorm.Model
-	World string
-	Pages []Page
+	World       string
+	Translation string
+	Pages       []Page
 }
 
 type Page struct {
@@ -45,148 +46,18 @@ func New() *DB {
 	return &DB{db: db}
 }
 
-func (d *DB) AddWord(word string, pages []Page) {
-	d.db.Create(&Word{World: word, Pages: pages})
-}
-
-func (d *DB) AddPage(page Page) {
-	d.db.Create(&page)
-}
-
-func (d *DB) AddImage(image Image) {
-	d.db.Create(&image)
-}
-
-func (d *DB) AddVideo(video Video) {
-	d.db.Create(&video)
-}
-
-func (d *DB) GetWord(word string) Word {
+func (d *DB) AddWord(word string, Translation string) uint {
+	// Проверяем есть ли уже такое слово в базе
 	var w Word
 	d.db.Where("world = ?", word).First(&w)
-	return w
-}
-
-func (d *DB) GetPage(id uint) Page {
-	var p Page
-	d.db.First(&p, id)
-	return p
-}
-
-func (d *DB) GetImage(id uint) Image {
-	var i Image
-	d.db.First(&i, id)
-	return i
-}
-
-func (d *DB) GetVideo(id uint) Video {
-	var v Video
-	d.db.First(&v, id)
-	return v
-}
-
-func (d *DB) Close() {
-	d.db.Close()
-}
-
-func (d *DB) GetPages() []Page {
-	var pages []Page
-	d.db.Find(&pages)
-	return pages
-}
-
-func (d *DB) GetImages() []Image {
-	var images []Image
-	d.db.Find(&images)
-	return images
-}
-
-func (d *DB) GetVideos() []Video {
-	var videos []Video
-	d.db.Find(&videos)
-	return videos
-}
-
-func (d *DB) GetWords() []Word {
-	var words []Word
-	d.db.Find(&words)
-	return words
-}
-
-func (d *DB) GetPagesByWord(word string) []Page {
-	var pages []Page
-	d.db.Where("word_id = ?", word).Find(&pages)
-	return pages
-}
-
-func (d *DB) GetImagesByPage(page Page) []Image {
-	var images []Image
-	d.db.Where("page_id = ?", page.ID).Find(&images)
-	return images
-}
-
-func (d *DB) GetVideosByPage(page Page) []Video {
-	var videos []Video
-	d.db.Where("page_id = ?", page.ID).Find(&videos)
-	return videos
-}
-
-func (d *DB) GetWordByPage(page Page) Word {
-	var word Word
-	d.db.First(&word, page.WordID)
-	return word
-}
-
-func (d *DB) GetPageByImage(image Image) Page {
-	var page Page
-	d.db.First(&page, image.PageID)
-	return page
-}
-
-func (d *DB) GetPageByVideo(video Video) Page {
-	var page Page
-	d.db.First(&page, video.PageID)
-	return page
-}
-
-func (d *DB) GetWordByImage(image Image) Word {
-	var page Page
-	d.db.First(&page, image.PageID)
-	return d.GetWordByPage(page)
-}
-
-func (d *DB) GetWordByVideo(video Video) Word {
-	var page Page
-	d.db.First(&page, video.PageID)
-	return d.GetWordByPage(page)
-}
-
-func (d *DB) GetImagesByWord(word string) []Image {
-	var images []Image
-	pages := d.GetPagesByWord(word)
-	for _, page := range pages {
-		images = append(images, d.GetImagesByPage(page)...)
+	if w.ID == 0 {
+		w = Word{World: word, Translation: Translation}
+		d.db.Create(&w)
 	}
-	return images
+	return w.ID
 }
 
-func (d *DB) GetVideosByWord(word string) []Video {
-	var videos []Video
-	pages := d.GetPagesByWord(word)
-	for _, page := range pages {
-		videos = append(videos, d.GetVideosByPage(page)...)
-	}
-	return videos
-}
-
-func (d *DB) GetPagesByImage(image Image) []Page {
-	var pages []Page
-	d.db.Where("id = ?", image.PageID).Find(&pages)
-	return pages
-}
-
-func (d *DB) GetPagesByVideo(video Video) []Page {
-	var pages []Page
-	d.db.Where("id = ?", video.PageID).Find(&pages)
-	return pages
+func (d *DB) AddPage(page Page, word_id uint) {
+	page.WordID = word_id
+	d.db.Create(&page)
 }
